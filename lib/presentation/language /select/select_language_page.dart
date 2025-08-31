3797b5647a911e78ce98f4070f9eeadfbaa2bddd
sd <zokirjonashiraliyev@mail.ru>
2025-08-31 19:08:08 +0500

    select language was added

---
+++ lib/presentation/language /select/select_language_page.dart
@@ -0,0 +1,17 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/cupertino.dart';
+
+@RoutePage()
+class SelectLanguagePage extends StatefulWidget {
+  const SelectLanguagePage({super.key});
+
+  @override
+  State<SelectLanguagePage> createState() => _SelectLanguagePageState();
+}
+
+class _SelectLanguagePageState extends State<SelectLanguagePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
