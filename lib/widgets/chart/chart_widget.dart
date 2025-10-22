25f32977a9a3d71ea646e91106d5111a37053f61
Nod1rbe <hasanovnodir2005@gmail.com>
2025-10-22 10:45:41 +0500

    changed chart widget

--- lib/widgets/chart/chart_widget.dart
+++ lib/widgets/chart/chart_widget.dart
@@ -10,20 +10,25 @@ class ChartWidget extends StatelessWidget {
   final ChartType type;
   final List<double> primaryValues;
   final double? target;
+
   const ChartWidget({super.key, required this.type, required this.primaryValues, this.target});
+
   @override
   Widget build(BuildContext context) {
     if (primaryValues.isEmpty) {
       return const SizedBox.shrink();
     }
+
     final average = primaryValues.reduce((a, b) => a + b) / primaryValues.length;
     final total = primaryValues.reduce((a, b) => a + b);
+
     final maxValue = [
       ...primaryValues,
       if (target != null) target!,
       average,
     ].reduce((a, b) => a > b ? a : b);
     final maxY = maxValue * 1.1;
+
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
@@ -108,11 +113,15 @@ class ChartWidget extends StatelessWidget {
                 final barColor = target != null && value < target!
                     ? context.colors.textSub
                     : context.colors.blueAccent;
+
+                // 🔥 Nol qiymatni ham rangli qilib ko‘rsatish uchun:
+                final displayValue = value == 0 ? maxY * 0.01 : value;
+
                 return BarChartGroupData(
                   x: index,
                   barRods: [
                     BarChartRodData(
-                      toY: value,
+                      toY: displayValue,
                       color: barColor,
                       width: type == ChartType.monthly ? 4 : 12,
                       borderRadius: BorderRadius.circular(2),
