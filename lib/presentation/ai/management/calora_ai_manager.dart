a77f5b7d44af19893fc07ca780afb3ba8822466a
sd <zokirjonashiraliyev@mail.ru>
2025-11-08 16:49:00 +0500

    there was changes in camera calora ai other classes

--- lib/presentation/ai/management/calora_ai_manager.dart
+++ lib/presentation/ai/management/calora_ai_manager.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:calora/presentation/ai/management/calora_ai_management.dart';
 import 'package:injectable/injectable.dart';
 import 'package:management/management.dart';
@@ -7,14 +9,22 @@ import 'package:permission_handler/permission_handler.dart';
 class CaloraAiManager extends Manager<CaloraAiState, CaloraAiEffect> {
   CaloraAiManager() : super(const CaloraAiState());
 
-  void openConfirmPage() {
-    publish(const CaloraAiEffect.showConfirmDialog());
-  }
-
-  Future<void> requestCameraPermission() async {
+  void openConfirmPage() async {
     final status = await Permission.camera.request();
     if (status.isGranted) {
       publish(const CaloraAiEffect.navigateToCamera());
+    } else {
+      publish(const CaloraAiEffect.showConfirmDialog());
+    }
+  }
+
+  Future<void> requestCameraPermission() async {
+    final status = await Permission.camera.status;
+
+    if (status.isGranted) {
+      publish(const CaloraAiEffect.navigateToCamera());
+    } else {
+      openAppSettings();
     }
   }
 }
