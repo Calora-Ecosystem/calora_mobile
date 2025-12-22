e734c3d0f89564d5c6f8898b44bfb0e2a5761f7a
shamsiddin <shamsiddinakhmedov3107@gmail.com>
2025-12-22 15:38:30 +0500

    Refactor: Update app package name and dependencies

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "ai.calora.calora"
+    namespace = "ai.calora.app"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -26,7 +26,7 @@ android {
     }
 
     defaultConfig {
-        applicationId = "ai.calora.calora"
+        applicationId = "ai.calora.app"
         minSdk = 29
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
