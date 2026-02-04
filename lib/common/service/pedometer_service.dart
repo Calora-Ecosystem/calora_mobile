498c4997763ee8f3dc5ec4f4adcbc8cb50bac4da
Habibullo <khabibullozokirov030@gmail.com>
2026-02-04 16:42:15 +0500

    wip

--- lib/common/service/pedometer_service.dart
+++ lib/common/service/pedometer_service.dart
@@ -1,4 +1,3 @@
-
 import 'dart:async';
 import 'dart:developer';
 import 'dart:io';
@@ -55,12 +54,12 @@ class PedometerService {
     return now
         .subtract(Duration(days: now.weekday - 1))
         .copyWith(
-      hour: 0,
-      minute: 0,
-      second: 0,
-      millisecond: 0,
-      microsecond: 0,
-    );
+          hour: 0,
+          minute: 0,
+          second: 0,
+          millisecond: 0,
+          microsecond: 0,
+        );
   }
 
   DateTime get _endOfWeek {
@@ -68,12 +67,12 @@ class PedometerService {
     return now
         .add(Duration(days: DateTime.daysPerWeek - now.weekday))
         .copyWith(
-      hour: 23,
-      minute: 59,
-      second: 59,
-      millisecond: 999,
-      microsecond: 999,
-    );
+          hour: 23,
+          minute: 59,
+          second: 59,
+          millisecond: 999,
+          microsecond: 999,
+        );
   }
 
   DateTime get _startOfMonth {
@@ -194,7 +193,7 @@ class PedometerService {
 
       _stepStreamSubscription?.cancel();
       _stepStreamSubscription = Pedometer().stepCountStream().listen(
-            (int steps) async {
+        (int steps) async {
           log('Real-time step count: $steps', name: 'PedometerService');
 
           if (!isStreamInitialized) {
@@ -252,7 +251,7 @@ class PedometerService {
     try {
       _pedestrianStatusSubscription?.cancel();
       _pedestrianStatusSubscription = Pedometer().pedestrianStatusStream().listen(
-            (PedestrianStatus status) {
+        (PedestrianStatus status) {
           log('Pedestrian status: $status', name: 'PedometerService');
           _emitStatus(status);
         },
@@ -289,7 +288,6 @@ class PedometerService {
   Future<int> getWeeklySteps() async => await getStepsForDateRange(_startOfWeek, _endOfWeek);
   Future<int> getMonthlySteps() async => await getStepsForDateRange(_startOfMonth, _endOfMonth);
 
-  // ✅ Yangi method: Date range uchun steps
   Future<Map<DateTime, int>> getDailyStepsForRange(DateTime fromDate, DateTime toDate) async {
     final Map<DateTime, int> result = {};
 
