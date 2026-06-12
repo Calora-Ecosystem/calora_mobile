50e42f838d19a232f5d2f99a24f3ee607b67b608
shamsiddin <shamsiddinakhmedov3107@gmail.com>
2026-06-12 10:14:32 +0500

    Update localization translations and bump app version

--- lib/common/gen/strings.dart
+++ lib/common/gen/strings.dart
@@ -10,7 +10,7 @@ part 'strings.g.dart';
 
 @SheetLocalization(
   docId: '12OIaFbGqovuAgHEcXFADUXPb5AqlyGASLpP3kHyFQtU',
-  version: 105,
+  version: 107,
   outDir: 'assets/localization',
   outName: 'translations.csv',
   preservedKeywords: [
